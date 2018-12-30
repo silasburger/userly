b8a2c911682388e1b0e113d28b879e559ad0e4c0
Silas Burger <ybsilas@gmail.com>
2018-12-30 14:15:13 -0800

    edited auth functions; removed isAdmin

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,8 @@ const {SECRET} = require('../config');
 async function ensureLoggedIn(req, res, next) {
   try {
     const tokenFromBody = req.body.token;
-    jwt.verify(tokenFromBody, SECRET);
+    const _token = jwt.verify(tokenFromBody, SECRET);
+    req.username = _token.username;
     next();
   } catch (e) {
     return next({ status: 401, message: "Unauthorized" });
@@ -14,19 +15,18 @@ async function ensureLoggedIn(req, res, next) {
 async function ensureCorrectUser(req, res, next) {
   try {
     const tokenFromBody = req.body.token;
-    const token = jwt.verify(tokenFromBody, SECRET);
-    
-    next();
+    const _token = jwt.verify(tokenFromBody, SECRET);
+    if(_token.username === req.params.username) {
+      next();
+    } else {
+      throw new Error();
+    }
   } catch (e) {
     return next({ status: 401, message: "Unauthorized" });
   }
 } 
 
-async function isAdmin(req, res, next) {
-  if()
-} 
-
 module.exports = {
-  isCorrectUser,
-  isAdmin
+  ensureLoggedIn,
+  ensureCorrectUser,
 }
